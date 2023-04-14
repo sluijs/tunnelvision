7ab403264cfaef152b057da22fd2b861ba03824a
Rogier van der Sluijs <rogier@vandersluijs.tech>
2023-04-14 12:27:50 -0700

    Added tunnel-vision server and PoC for plotting

---
+++ tunnelvision/definitions.py
@@ -0,0 +1,5 @@
+import os
+
+__all__ = ["ROOT_DIR"]
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
--- tunnelvision/experimental/plot.py
+++ tunnelvision/experimental/plot.py
@@ -1,10 +1,16 @@
+import asyncio
+import json
 import os
 import socket
 import subprocess
+import time
 from typing import Tuple
 
 import numpy as np
+import websockets
 from IPython.display import IFrame, display
+
+from tunnelvision.definitions import ROOT_DIR
 
 # Name of the localhost.
 LOCALHOST_NAME = os.getenv("TUNNELVISION_HOST", "localhost")
@@ -21,9 +27,17 @@ class Axis:
             raise RuntimeError("Tunnelvision can only be used in an IPython session within Chrome.")
 
         self.port = _get_first_available_port()
+        self.server_path = os.path.join(ROOT_DIR, "bin/tunnelvision-server")
+        self.dist_path = os.path.join(ROOT_DIR, "bin/dist")
         self.process = None
 
-    def show(self, x: np.ndarray, *, figsize: Tuple[int, int] = (514, 574), port=None):
+    def show(
+        self,
+        x: np.ndarray,
+        *,
+        figsize: Tuple[int, int] = (512, 512),
+        port=None,
+    ):
         """Display a multi-dimensional array.
 
         Args:
@@ -37,22 +51,25 @@ class Axis:
         if x.ndim != 5:
             raise ValueError("Only 5-dimensional arrays are supported [BZYXC].")
 
-        w, h = figsize
+        h, w = figsize
         if w <= 256 or h <= 256:
             raise ValueError("The figure size must be at least 256x256 pixels.")
 
-        uri = f"http://{LOCALHOST_NAME}:{port or self.port}/ws"
-        print(f"--- Opening viewer at {uri} ...")
-
+        uri = f"http://{LOCALHOST_NAME}:{port or self.port}?port={port or self.port}"
         self.process = subprocess.Popen(
-            ["tunnelvision", "--port", str(self.port)],
+            [self.server_path, "--port", str(self.port), "-d", str(self.dist_path)],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
 
         # Display the viewer
-        display(IFrame(uri, width=w, height=h))
+        display(IFrame(uri, width=w + 62, height=h + 2))
 
+        # Wait for the server to start, and send  the array
+        ws_uri = f"ws://{LOCALHOST_NAME}:{port or self.port}/ws"
+        _ = asyncio.create_task(_send_array(x, ws_uri))
+
+        # Return the object with the process, so that it can be closed
         return self
 
 
@@ -115,3 +132,36 @@ def _get_first_available_port(initial: int = 49152, final: int = 65535) -> int:
     raise OSError(
         "All ports from {} to {} are in use. Please close a port.".format(initial, final - 1)
     )
+
+
+async def _send_array(x: np.ndarray, uri: str, timeout: int = 5, interval: int = 0.1):
+    # TODO: establish a handshake between the frontend client through the backend server
+    # For now, we just wait a bit and hope that the *front-end* has loaded and is ready to receive
+    # the array through the websocket.
+    time.sleep(0.25)
+
+    # TODO: ping the server to see if it is ready to receive the array
+    start_time = time.time()
+    while True:
+        # Try to send the array, and if it fails, wait a bit and try again
+        try:
+            async with websockets.connect(uri) as websocket:
+                # Send the header with a prefix signaling that it is JSON
+                header = "JSON" + json.dumps({"shape": x.shape, "dtype": x.dtype.name})
+                header = header.encode("utf-8")
+                await websocket.send(header)
+
+                # Send the array
+                await websocket.send(x.tobytes())
+                await websocket.close(reason="Goodbye!")
+
+            # Break out of the loop, since the array was sent
+            break
+
+        except ConnectionRefusedError:
+            pass
+
+        if time.time() - start_time > timeout:
+            raise TimeoutError("The server did not start in time.")
+
+        time.sleep(interval)
