17462a9ed3a430228ff1eca0c772ca9642e60adf
Rogier van der Sluijs <rogier@vandersluijs.tech>
2023-04-25 09:23:01 -0700

    Fix: implemented spinners

--- tunnelvision/__init__.py
+++ tunnelvision/__init__.py
@@ -1,8 +1,8 @@
 """Tunnelvision"""
 
-from tunnelvision.plot import Axes, show  # noqa: F401
+from tunnelvision.plot import Axes, imshow, show  # noqa: F401
 
-__all__ = ["plot", "Axes", "show"]
+__all__ = ["plot", "Axes", "show", "imshow"]
 
 # This line will be read by setup.py
-__version__ = "0.3.0"
+__version__ = "0.3.1"
