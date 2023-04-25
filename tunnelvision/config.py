99acec65dfe14edb6415dc6909ed1a73709a55f2
Rogier van der Sluijs <rogier@vandersluijs.tech>
2023-04-24 21:20:40 -0700

    Temporarily removed stderr logs

--- tunnelvision/config.py
+++ tunnelvision/config.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 from dataclasses import dataclass
+from typing import Optional
 
 import yaml
 
@@ -23,14 +24,13 @@ class Config(metaclass=Singleton):
 
     # The port to use for tunnelvision-server, defaults to the first available port.
     # NB: set this port when working with VS Code Remote.
-    port: int = None
+    port: Optional[int] = None
 
     # How long to wait for the front-end to respond before giving up.
     timeout: int = 5
 
-    # The path to the stdout and stderr logs of tunnelvision-server.
+    # Paths to the stdout and stderr logs of tunnelvision-server.
     log_stdout: str = os.path.join(os.path.expanduser("~"), ".cache", "tunnelvision", "server.out")
-    log_stderr: str = os.path.join(os.path.expanduser("~"), ".cache", "tunnelvision", "server.err")
 
     # Hydrate the config from a YAML file.
     def __post_init__(self):
@@ -46,9 +46,6 @@ class Config(metaclass=Singleton):
         # Remove the logs
         if os.path.exists(self.log_stdout):
             os.remove(self.log_stdout)
-
-        if os.path.exists(self.log_stderr):
-            os.remove(self.log_stderr)
 
 
 config = Config()
